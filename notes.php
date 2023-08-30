9560cb63c085011e8950bae6c0f73ea614c5a5ef
madgear <anthony.monastrial@gmail.com>
2023-08-31 07:39:09 +0800

    Update notes.php

--- notes.php
+++ notes.php
@@ -1,4 +1,31 @@
 <?php
 echo "test";
 
+
+echo "https://github.com/mishoo/UglifyJS2/releases";
+
+
+
 ?>
+
+
+<script src="path/to/uglify-js.js"></script>
+
+
+<script src="path/to/uglify-js.js"></script>
+<script>
+  const code = `
+    function greet(name) {
+        console.log('Hello, ' + name);
+    }
+
+    greet('Alice');
+  `;
+
+  const options = {
+    // Options for minification and obfuscation
+  };
+
+  const result = UglifyJS.minify(code, options);
+  console.log(result.code);
+</script>
