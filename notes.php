f50e4ff7c11518a3883f29e9fac3452e30e2c098
madgear <anthony.monastrial@gmail.com>
2024-01-25 12:34:36 +0800

    Update notes.php

--- notes.php
+++ notes.php
@@ -1,3 +1,22 @@
+
+Sub SaveClipboardImageToLocal()
+    Dim objData As MSForms.DataObject
+    Set objData = New MSForms.DataObject
+
+    ' Get data from the Clipboard
+    objData.GetFromClipboard
+
+    ' Check if the Clipboard contains an image
+    If objData.GetFormat(1) Then
+        ' Save the image to a local file
+        Open "C:\Path\To\Your\Local\File.jpg" For Binary Access Write As #1
+        Put #1, , objData.GetData(1)
+        Close #1
+    Else
+        MsgBox "No image found in the Clipboard.", vbExclamation
+    End If
+End Sub
+
 Sub SaveClipboardImageToLocal()
     Dim objData As DataObject
     Set objData = New DataObject
